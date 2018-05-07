0cad8be54043788362f4f196f547b9cc261dec42
Abhishek Girish <agirish@apache.org>
2018-05-07 14:52:10 -0700

    Update Constants.java

--- src/main/java/com/mapr/utils/Constants.java
+++ src/main/java/com/mapr/utils/Constants.java
@@ -4,7 +4,7 @@ package com.mapr.utils;
  * Created by aravi on 10/13/17.
  */
 public class Constants {
-    public static final String TABLE_NAME  = "/business_table";
+    public static final String TABLE_NAME  = "/drill/testdata/ojai/business_table";
     public static final String CONNECTION_URL = "ojai:mapr:";
     public static final int TIMEOUT_IN_MILLIS = 60000; //60 seconds
 }
